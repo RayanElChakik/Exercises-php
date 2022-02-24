832eae4621bf4e2e43c3a87340b15eb720a04e64
Rayan El Chakik <rayan.elchakik@gmail.com>
2022-02-24 12:20:25 +0000

    Step_4

---
+++ php-basics-01/Step_4.php
@@ -0,0 +1,11 @@
+<?php
+$name = 'Rayan';
+$position = "a junior web developer";
+$github_url = "https://github.com/RayanElChakik";
+echo "Hello, I'm " .$name.", I'm " .$position. " please check my github link " .$github_url. "\n";
+echo "Hello, I'm $name, I'm $position please check my github link $github_url \n";
+echo 'Hello, I\'m ' .$name.', I\'m ' .$position. ' please check my github link ' .$github_url. "\n";
+echo 'Hello, I\'m $name, I\'m $position please check my github link $github_url \n';
+echo "Hello, I'm {$name}, I'm {$position} please check my github link {$github_url} \n";
+
+?>
