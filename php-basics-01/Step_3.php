a405a5487d85385a815017402f41ad2c34a7da20
Rayan El Chakik <rayan.elchakik@gmail.com>
2022-02-24 11:18:16 +0000

    Step_3

---
+++ php-basics-01/Step_3.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Rayan";
+$email = "rayan.elchaki@gmail.com";
+$height = 10;
+$width = 20;
+$area = $height * $width;
+echo "My name is $name and my email is $email\n";
+echo "The outputed area is $area";
+?>
