a7b5387b3b7b3bcd67a7685d55ad165b92eeb68a
Rayan El Chakik <rayan.elchakik@gmail.com>
2022-02-24 12:27:22 +0000

    Step_4

--- php-basics-01/Step_4.php
+++ php-basics-01/Step_4.php
@@ -5,7 +5,7 @@ $github_url = "https://github.com/RayanElChakik";
 echo "Hello, I'm " .$name.", I'm " .$position. " please check my github link " .$github_url. "\n";
 echo "Hello, I'm $name, I'm $position please check my github link $github_url \n";
 echo 'Hello, I\'m ' .$name.', I\'m ' .$position. ' please check my github link ' .$github_url. "\n";
-echo 'Hello, I\'m $name, I\'m $position please check my github link $github_url \n';
+echo 'Hello, I\'m ' .$name. ', I\'m ' .$position. 'please check my github link ' .$github_url. "\n";
 echo "Hello, I'm {$name}, I'm {$position} please check my github link {$github_url} \n";
 
 ?>
