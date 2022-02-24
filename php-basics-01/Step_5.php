f45e694bf1db0e19c4df4bef2885b07bf51bd3d6
Rayan El Chakik <rayan.elchakik@gmail.com>
2022-02-24 14:28:37 +0000

    Step_5

---
+++ php-basics-01/Step_5.php
@@ -0,0 +1,13 @@
+<?php
+$course_name = "PHP";
+$enrolled_students = 10;
+$price = 25.99;
+$on_discount = true;
+
+echo "Course title: $course_name  
+Enrolled Studnets: $enrolled_students 
+Course price: $price $ USD;
+Course on discount:";
+echo $on_discount ? 'Yes' : 'No';
+
+?>
