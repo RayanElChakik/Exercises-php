f041eadc759e1021653932bcf11e69939cff856c
Rayan El Chakik <rayan.elchakik@gmail.com>
2022-02-26 20:12:49 +0000

    Step_8

--- php-basics-02/Step_8.php
+++ php-basics-02/Step_8.php
@@ -2,11 +2,13 @@
 function powerOfTwo($num){
     if($num == 1){
         return "True, it is power of two";
-    }else if(($num % 2) != 0 || $num === 0  || $num === 1){
+    }else if(($num % 2) != 0 || $num === 0){
         return "False, it is not power of two";
     }else return "True is power of two";
 }
 echo powerOfTwo(4), "\n";
 echo powerOfTwo(3), "\n";
 echo powerOfTwo(1), "\n";
+echo powerOfTwo(16), "\n";
+echo powerOfTwo(0), "\n";
 ?>
