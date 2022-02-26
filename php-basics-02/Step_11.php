df3bf1b77558188e6f1350f742af6bae88b34ad7
Rayan El Chakik <rayan.elchakik@gmail.com>
2022-02-26 21:33:16 +0000

    Step_11

--- php-basics-02/Step_11.php
+++ php-basics-02/Step_11.php
@@ -1,6 +1,6 @@
 <?php
 function isPrime($num){
-    if ($num == 1)
+    if ($num === 1 || $num === 0)
     return "{$num} is not a prime number";
     for ($i = 2; $i <= $num/2; $i++){
         if ($num % $i == 0) return "{$num} is not a prime number";
@@ -11,4 +11,5 @@ echo isPrime(3), "\n";
 echo isPrime(1) , "\n";
 echo isPrime(5) , "\n";
 echo isPrime(13) , "\n";
+echo isPrime(0)  , "\n";
 ?>
