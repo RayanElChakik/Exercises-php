b579a3e14865696b50aa0dc1e11b83ddac16790f
Rayan El Chakik <rayan.elchakik@gmail.com>
2022-02-27 11:35:24 +0000

    For_loop Exercise 3

---
+++ php-basics-03/for_loop_ex3.php
@@ -0,0 +1,17 @@
+<?php
+function starPattern($n){
+    for($i=1; $i<=$n;$i++){
+        for($j=1; $j<=$i;$j++){
+            echo "*";
+        }    
+        echo PHP_EOL;
+    }
+    for($i=$n; $i>=1;$i--){
+        for($j=1; $j<=$i;$j++){
+            echo "*";
+        }    
+        echo PHP_EOL;
+    }
+}
+echo starPattern(5);
+?>
